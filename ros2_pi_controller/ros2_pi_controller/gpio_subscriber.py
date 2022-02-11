08118530a5f2c89dc32c1c17bc506f9e0d83a3ec
Andy Ponce <andy.ponce@outlook.com>
2022-02-11 00:14:08 -0500

    reverted accidental modification of minimal_subscriber and fixed issue with gpio_subscriber

--- ros2_pi_controller/ros2_pi_controller/gpio_subscriber.py
+++ ros2_pi_controller/ros2_pi_controller/gpio_subscriber.py
@@ -13,15 +13,22 @@
 # limitations under the License.
 
 import rclpy
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+
 from rclpy.node import Node
 
 from std_msgs.msg import String
 
 
-class MinimalSubscriber(Node):
+class GpioSubscriber(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__('gpio_subscriber')
         self.subscription = self.create_subscription(
             String,
             'topic',
@@ -31,20 +38,37 @@ class MinimalSubscriber(Node):
 
     def listener_callback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg.data)
+        if 'ON' in msg.data:
+            if '24' in msg.data:
+                GPIO.output(24, GPIO.HIGH)  #turn on the LED
+            elif '23' in msg.data:
+                GPIO.output(23, GPIO.HIGH)  #turn on the LED
+            elif '22' in msg.data:
+                GPIO.output(22, GPIO.HIGH)  #turn on the LED
+        elif 'OFF' in msg.data:
+            if '24' in msg.data:
+                GPIO.output(24, GPIO.LOW)  #turn off the LED
+            elif '23' in msg.data:
+                GPIO.output(23, GPIO.LOW)  #turn off the LED
+            elif '22' in msg.data:
+                GPIO.output(22, GPIO.LOW)  #turn off the LED
+
+
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_subscriber = MinimalSubscriber()
+    gpio_subscriber = GpioSubscriber()
 
-    rclpy.spin(minimal_subscriber)
+    rclpy.spin(gpio_subscriber)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+    gpio_subscriber.destroy_node()
     rclpy.shutdown()
+    GPIO.cleanup()                  #clean up all the ports used
 
 
 if __name__ == '__main__':
